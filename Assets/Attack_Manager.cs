70b0528b551c9e7a724b38889a605873608253ae
VioletViolence <brosinskiy@gmail.com>
2019-02-16 01:30:39 +0200

    Basic Scripts

---
+++ Assets/Attack_Manager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_Manager : MonoBehaviour
+{
+    [SerializeField] GameObject Bullet;
+    private float AttackSpeed;
+    private int AttackDamage;
+    private int BulletSpeed;
+    private int Health;
+    private int Armor;
+    private int MagicResist;
+    private bool isMagical;
+    private GameObject Enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Attack", Time.deltaTime, AttackSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Attack()
+    {
+        if(gameObject.tag == "Player")
+        {
+            Enemy = GameObject.FindGameObjectWithTag("Enemy");
+        }
+        else
+        {
+            Enemy = GameObject.FindGameObjectWithTag("Player");
+        }
+        GameObject newBullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+        newBullet.GetComponent<Bullet>().SetEnemy(Enemy);
+        newBullet.GetComponent<Bullet>().SetSpeed(BulletSpeed);
+
+    }
+
+
+    // Getters and Setters
+    public float GetAttackSpeed()
+    {
+        return AttackSpeed;
+    }
+    public void SetAttackSpeed(float atk)
+    {
+        AttackSpeed = atk;
+    }
+    public int GetAttackDamage()
+    {
+        return AttackDamage;
+    }
+    public void SetAttackDamage(int dmg)
+    {
+        AttackDamage = dmg;
+    }
+    public int GetHealth ()
+    {
+        return Health;
+    }
+    public void SetHealth(int HP)
+    {
+        Health = HP;
+    }
+    public int GetBulletSpeed()
+    {
+        return BulletSpeed;
+    }
+    public void SetBulletSpeed(int bullSp)
+    {
+        AttackSpeed = bullSp;
+    }
+    public int GetArmor()
+    {
+        return Armor;
+    }
+    public void SetArmor(int arm)
+    {
+        Armor = arm;
+    }
+    public int GetMagicResist()
+    {
+        return MagicResist;
+    }
+    public void SetMagicResist(int magRes)
+    {
+        MagicResist = magRes
+;
+    }
+    public bool GetIsMagical()
+    {
+        return isMagical;
+    }
+    public void SetIsMagical(bool isM)
+    {
+        isMagical = isM;
+    }
+}
---
+++ Assets/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] Rigidbody RigThis;
+    private GameObject Enemy;
+    private int Speed;
+
+    public void SetEnemy(GameObject E)
+    {
+        Enemy = E;
+    }
+
+    public void SetSpeed(int S)
+    {
+        Speed = S;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RigThis.velocity = Enemy.transform.position * Speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
